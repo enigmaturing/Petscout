e4ae307db950803f555828c67247c7bbdb97b3d4
Javier Gonzalez <javier.glez.martin@gmail.com>
2019-12-04 21:18:44 +0100

    Simple FEED ME/STOP screen implemented

--- Petscout/Petscout/MainPage.xaml.cs
+++ Petscout/Petscout/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -16,6 +17,33 @@ namespace Petscout
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void ButtonON_Clicked(object sender, EventArgs e)
+        {
+            toggleMotor("on");
+        }
+
+        private void ButtonOFF_Clicked(object sender, EventArgs e)
+        {
+            toggleMotor("off");
+        }
+
+        private async void toggleMotor(string s)
+        {
+            if (tokenEntry.Text == null)
+            {
+                await DisplayAlert("Alert", "Insert a valid token first!", "OK");
+            }
+            else
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var formcontent = new FormUrlEncodedContent(new[] { new KeyValuePair<string, string>("arg", s) });
+                    var request = await httpClient.PostAsync("https://api.particle.io/v1/devices/e00fce68ba9a1f5ea4870186/motorToggle?access_token=" + tokenEntry.Text.ToString(), formcontent);
+                }
+            }
+
         }
     }
 }
